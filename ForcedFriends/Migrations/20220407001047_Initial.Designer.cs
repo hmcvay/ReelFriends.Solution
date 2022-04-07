5eb7c30e6c01cf1e08aff738da911904b9e74f14
hmcvay <hayleymcvay8@gmail.com>
2022-04-06 18:14:27 -0700

    add favicon

--- ForcedFriends/Migrations/20220407001047_Initial.Designer.cs
+++ ForcedFriends/Migrations/20220407001047_Initial.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ForcedFriends.Migrations
 {
     [DbContext(typeof(ForcedFriendsContext))]
-    [Migration("20220406203742_Initial")]
+    [Migration("20220407001047_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
